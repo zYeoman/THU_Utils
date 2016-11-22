3a1e8d0eac1d5564f1ab78f02b750269ff6223ac
Yongwen Zhuang <zyeoman@163.com>
2016-11-23 00:24:45 +0800

    :sparkles: Add user.py to store and load user info.
    添加第一个工具，存储用户名密码。
    
    存储用户名密码，供其他工具使用

---
+++ thu_utils/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from thu_utils.user import User
---
+++ thu_utils/user.py
@@ -0,0 +1,50 @@
+import os
+import os.path
+import pickle
+import getpass
+
+class User(object):
+
+    """Contain username and password."""
+
+    def __init__(self, path='.thu'):
+        """
+        init User
+
+        :path: where to store username and password.
+
+        """
+        self._path = path
+        home = os.path.expanduser('~')
+        self._filename = os.path.join(home, self._path)
+        self._data = self._load()
+
+    def _load(self):
+        if not os.path.exists(self._filename):
+            self.set_user()
+        with open(self._filename, 'rb') as file:
+            return pickle.load(file)
+
+
+    def _store(self):
+        with open(self._filename, 'wb') as file:
+            pickle.dump(self._data, file)
+        os.chmod(self._filename, 0o600)
+
+    def set_user(self):
+        username = input('Username: ').encode()
+        password = getpass.getpass().encode()
+        self._data['username'] = username
+        self._data['password'] = password
+        self._store()
+
+    @property
+    def username(self):
+        return self._data['username']
+
+    @property
+    def password(self):
+        return self._data['password']
+
+    def show(self):
+        print(self._data['username'].decode())
