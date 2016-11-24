7c585b59037eb9f692806124789d8b63567a629d
Yongwen Zhuang <zyeoman@163.com>
2016-11-24 21:08:27 +0800

    :ribbon: Use requests
    使用第三方库requests，适配python2
    
    1. requests 好用
    2. requests 同时支持2/3

--- thu_utils/net.py
+++ thu_utils/net.py
@@ -3,8 +3,7 @@
 
 from hashlib import md5
 from collections import namedtuple
-from urllib.request import urlopen, Request
-from urllib.parse import urlencode
+import requests
 
 from .user import User
 
@@ -14,18 +13,19 @@ class Net(object):
 
     def __init__(self, user=None):
         self._user = user if user is not None else User()
+        self._session = requests.session()
         self._base = 'http://net.tsinghua.edu.cn/'
         self._login_url = self._base + 'do_login.php'
         self._net_usage = namedtuple('NetUsage', 'ip user traffic timelen')
 
     def show(self):
-        req = Request(self._login_url, b'action=check_online')
-        resp = urlopen(req).read().decode()
-        print(resp)
-        if resp != 'not_online':
-            req = Request(self._base + 'rad_user_info.php', b'')
-            resp = urlopen(req).read().decode()
-            info = resp.split(',')
+        req = self._session.post(self._login_url, {'action':'check_online'})
+        req_content = req.content
+        print(req_content)
+        if req_content != b'not_online':
+            req = self._session.post(self._base + 'rad_user_info.php')
+            req_content = req.content
+            info = req_content.split(b',')
             info = self._net_usage(*[
                 info[8], info[0], int(info[6]) / 1000000000,
                 int(info[2]) - int(info[1])
@@ -33,16 +33,16 @@ class Net(object):
             print(info)
 
     def login(self):
-        data = urlencode({
+        data = {
             'action': 'login',
             'username': self._user.username,
             'password': '{MD5_HEX}' + md5(self._user.password).hexdigest(),
             'ac_id': 1
-        })
-        req = Request(self._login_url, data.encode())
-        return urlopen(req).read().decode()
+        }
+        req = self._session.post(self._login_url, data)
+        return req.content
 
     def logout(self):
-        date = urlencode({'action': 'logout'})
-        req = Request(self._login_url, date.encode())
-        return urlopen(req).read().decode()
+        date = {'action': 'logout'}
+        req = self._session.post(self._login_url, date)
+        return req.content
