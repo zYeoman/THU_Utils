8f42106d71a596e3e640b45d4b172702f15f669f
Yongwen Zhuang <zyeoman@163.com>
2016-12-04 16:11:26 +0800

    :sparkles: test.py and update version in __init__.py
    先写代码后写测试，这根本不是TDD！
    
    希望能够TDD。。。

---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import thu_utils
+
+class TestTHU(unittest.TestCase):
+
+    """Unittest for thu_utils"""
+
+    def test_user(self):
+        thu_utils.user.input = lambda _:'test'
+        thu_utils.user.getpass.getpass = lambda :'test'
+        user = thu_utils.User('.test_thu')
+        self.assertEqual(user.username, b'test')
+        self.assertEqual(user.password, b'test')
+        user.del_user()
+
+    def test_net(self):
+        net = thu_utils.Net()
+        net.logout()
+        self.assertEqual(net.login(), 'Login is successful.')
+        self.assertEqual(net.login(), 'IP has been online, please logout.')
+        net.show()
+        self.assertEqual(net.logout(), 'Logout is successful.')
+        self.assertEqual(net.logout(), 'You are not online.')
+        net.show()
+
+    def test_learn(self):
+        learn = thu_utils.Learn()
