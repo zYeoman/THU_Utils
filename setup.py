91faa3d8248625ba15f2c3a8dc72d3ccae1e06a0
Yongwen Zhuang <zyeoman@163.com>
2016-12-09 23:12:18 +0800

    :sparkles: Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+__author__  = "Yongwen Zhuang (zhuangyw.thu@gmail.com)"
+__version__ = "0.0.3"
+__license__ = "GPLv3"
+__doc__     = "THU Utils: Tools for tsinghua students"
+
+setup(
+    name        = "thu_utils",
+    version     = __version__,
+    description = __doc__,
+    author      = __author__,
+    license     = __license__,
+    package     = find_packages(),
+    install_requires = [
+        'lxml',
+        'bs4'
+    ],
+)
+
