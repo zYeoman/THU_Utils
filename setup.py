0fdde34c9f8f5c9b13421ee147f258e7d56477aa
Yongwen Zhuang <zyeoman@163.com>
2016-12-10 19:45:30 +0800

    :bookmark: v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 __author__  = "Yongwen Zhuang (zhuangyw.thu@gmail.com)"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __license__ = "GPLv3"
 __doc__     = "THU Utils: Tools for tsinghua students"
 
--- thu_utils/__init__.py
+++ thu_utils/__init__.py
@@ -5,7 +5,7 @@ Tools for tsinghua students.
 """
 
 __author__ = "Yongwen Zhuang (zhuangyw.thu@gmail.com)"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __copyright__ = "Copyright (c) 2016 Yongwen Zhuang"
 __license__ = "GPLv3"
 from .user import User
