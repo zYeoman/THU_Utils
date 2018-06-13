60dd592018ea12bef4a9f1a54d29906c8245f3ab
Yongwen Zhuang <zyeoman@163.com>
2018-06-13 23:52:35 +0800

    :books: Setup.py v0.0.6

--- setup.py
+++ setup.py
@@ -1,17 +1,50 @@
-"""THU Utils Tools for tsinghua students """
+from io import open
 
 from setuptools import setup
 from setuptools import find_packages
 
-__author__ = "Yongwen Zhuang (zhuangyw.thu@gmail.com)"
-__version__ = "0.0.4"
-__license__ = "GPLv3"
+with open('thu_utils/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
 
 setup(
-    name="thu_utils",
-    version=__version__,
-    description=__doc__,
-    author=__author__,
-    license=__license__,
+    name='thu_utils',
+    version=version,
+    description='THU Utils Tools for tsinghua students ',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Yongwen Zhuang',
+    author_email='zyeoman@163.com',
+    maintainer='Yongwen Zhuang',
+    maintainer_email='zyeoman@163.com',
+    url='https://github.com/zYeoman/userpass',
+    license='GPLv3',
+
+    keywords=[
+        'spider',
+        'utils',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3 License',
+        'Natural Language :: Chinese',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
     package=find_packages(),
-    install_requires=['lxml', 'bs4', 'prettytable', 'requests'], )
+    install_requires=['lxml', 'bs4', 'prettytable', 'requests'],
+
+)
--- thu_utils/__init__.py
+++ thu_utils/__init__.py
@@ -4,10 +4,7 @@ Yongwen Zhuang
 Tools for tsinghua students.
 """
 
-__author__ = "Yongwen Zhuang (zhuangyw.thu@gmail.com)"
-__version__ = "0.0.5"
-__copyright__ = "Copyright (c) 2016-2017 Yongwen Zhuang"
-__license__ = "GPLv3"
+__version__ = "0.0.6"
 from .net import Net
 from .learn import Semester
 from .usereg import Usereg
